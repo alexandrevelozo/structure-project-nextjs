3cde5e33828db4b787f282f60af71193ab59e3f0
alexandrevelozo <alexandrevelozo4@gmail.com>
2020-08-26 21:29:58 -0300

    project-next

---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    font-size: 3.37em;
+    color: ${props => props.theme.colors.primary};
+    margin-top: 40px;
+  }
+
+  p {
+    margin-top: 24px;
+    font-size: 1.5em;
+    line-height: 32px;
+  }
+`
